d16680142158c9975cabfd18f2748374eedf68cb
ralskwo <ralskwo@gmail.com>
2024-10-24 23:24:53 +0900

    Score Part Update

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,12 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    StartCrashSequence();
+    //    //Debug.Log(collision.gameObject.name);
+    //}
+
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(collision.gameObject.name);
+        StartCrashSequence();
+    }
+
+    private void StartCrashSequence()
+    {
+        crashVFX.Play();
+
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
+
+        foreach(var child in transform.GetComponentsInChildren<MeshRenderer>())
+            child.enabled = false;
+
+
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 15;
+
+    ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
     private void OnParticleCollision(GameObject other)
     {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    private void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(this.gameObject);
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 }
